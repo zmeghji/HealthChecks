c44e2a8f90e2d7e79c24e1eb85f41b4f0e8ffcd2
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-09-10 17:16:40 -0400

    implemented health check

---
+++ HealthChecks/ApiHealthCheck.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecks
+{
+    public class ApiHealthCheck: IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                Random rand = new Random();
+
+                if (rand.Next(0, 2) == 0)
+                {
+                    return new HealthCheckResult(HealthStatus.Healthy, "Succeeded");
+                }
+                else
+                {
+                    return new HealthCheckResult(HealthStatus.Unhealthy, "Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                return new HealthCheckResult(HealthStatus.Unhealthy, "Failed", ex);
+            }
+
+        }
+
+       
+    }
+}
---
+++ HealthChecks/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HealthChecks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHealthChecks().AddCheck("ApiCheck", new ApiHealthCheck());
+            services.AddCors(options =>
+            {
+                options.AddPolicy("allowcaller", (builder) =>
+                {
+                    builder.WithOrigins("http://localhost:53301");
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("allowcaller");
+            app.UseHealthChecks("/hc");
+            
+            app.UseMvc();
+        }
+    }
+}
